e6ae8d465d9ba10da9b71997d6e57d75de828b5d
Jlucky94 <lucky10291994@gmail.com>
2023-03-08 14:08:11 +0300

    Samurai 94

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from "./Redux/redux-store";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    // <BrowserRouter>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
         <Provider store={store}>
-        <App/>
-    </Provider>
-    </HashRouter>
-    // </BrowserRouter>
+            <App/>
+        </Provider>
+    </BrowserRouter>
 )
 
 
